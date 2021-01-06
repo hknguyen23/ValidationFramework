5bdfda94b61843cf5afa2fe3e5493aad77083f43
Hồ Phan Minh <38271639+Akashi195@users.noreply.github.com>
2021-01-06 22:47:55 +0700

    update size annotation
    update size annotation

--- src/com/company/Function/SizeValidate.java
+++ src/com/company/Function/SizeValidate.java
@@ -5,7 +5,13 @@ import com.company.FunctionManager.AttributeObject;
 public class SizeValidate extends ValidateFunction {
     @Override
     public Boolean isValid(Object value, AttributeObject attribute) {
-        return ((((int) value) >= (int) attribute.getAttribute("min")) &&
-                  ((int) value <= (int) attribute.getAttribute("max")));
+        if(value instanceof Integer){
+            return ((((int) value) >= (int) attribute.getAttribute("min")) &&
+                    ((int) value <= (int) attribute.getAttribute("max")));
+        } else if(value instanceof String){
+            return ((value.toString().length() >= (int) attribute.getAttribute("min")) &&
+                    (value.toString().length() <= (int) attribute.getAttribute("max")));
+        }
+        return false;
     }
 }
