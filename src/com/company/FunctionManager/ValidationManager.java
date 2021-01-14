3da18c5ff30e2b5261df6516e8135d77202e8202
Hồ Phan Minh <38271639+Akashi195@users.noreply.github.com>
2021-01-14 23:19:57 +0700

    Refactor code
    Refactor code

--- src/com/company/FunctionManager/ValidationManager.java
+++ src/com/company/FunctionManager/ValidationManager.java
@@ -1,26 +1,17 @@
 package com.company.FunctionManager;
 
-import com.company.Constant.ValidatorStrategy;
-import com.company.validationStrategy.FailFastValidationStrategy;
-import com.company.validationStrategy.NormalValidationStrategy;
+
 import com.company.validationStrategy.ValidationStrategy;
 
-import java.util.HashMap;
 import java.util.List;
 
 public class ValidationManager {
 
 	private ValidationStrategy strategy = null;
-	private static HashMap<ValidatorStrategy, ValidationStrategy> list = new HashMap<>();
-
-	static {
-		list.put(ValidatorStrategy.NORMAL_VALIDATOR, NormalValidationStrategy.getInstance());
-		list.put(ValidatorStrategy.FAST_VALIDATOR, FailFastValidationStrategy.getInstance());
-	}
 
 	public static ValidationManager createValidatorStrategyByName(String nameStrategy){
 		ValidationManager validationManager = new ValidationManager();
-		validationManager.setStrategy(getValidationStrategy(nameStrategy));
+		validationManager.setStrategy(StrategyManager.getValidationStrategy(nameStrategy));
 		return validationManager;
 	}
 
@@ -33,20 +24,11 @@ public class ValidationManager {
 	}
 
 	public void updateStrategy(String name){
-		setStrategy(getValidationStrategy(name));
+		setStrategy(StrategyManager.getValidationStrategy(name));
 	}
 
 	public List<String> validate(Object object) {
 		return strategy.validate(object);
-	}
-
-
-	private static ValidationStrategy getValidationStrategy(String name){
-		ValidatorStrategy validatorStrategy = ValidatorStrategy.isValid(name);
-		if(list.containsKey(validatorStrategy)){
-			return list.get(validatorStrategy);
-		}
-		return list.get(ValidatorStrategy.NORMAL_VALIDATOR);
 	}
 
 	private void setStrategy(ValidationStrategy strategy) {
